58db0183a41a891fb2e60c6ed43a976e39d30859
Дарья <dashaptashuk@gmail.com>
2025-11-21 18:29:18 +0300

    initial implementation of the theme

--- js/calc.js
+++ js/calc.js
@@ -63,6 +63,9 @@ document.querySelector('.calc__buttons').onclick = (event) => {
     return
   }
   if (actions.includes(key)) {
+    if (numFirst === '' && key !== '+/-') {
+      return
+    }
     if (key === '+/-') {
       toggleSign()
     } else if (key === '%') {
@@ -146,3 +149,19 @@ function handlePoint() {
   }
   scrollToLeft()
 }
+
+const themeToggle = document.querySelector('.theme-toggle__checkbox')
+const bodyEl = document.body
+const savedTheme = localStorage.getItem('theme') || 'dark'
+bodyEl.setAttribute('data-theme', savedTheme)
+themeToggle.checked = savedTheme === 'light'
+
+themeToggle.addEventListener('change', function () {
+  if (this.checked) {
+    bodyEl.setAttribute('data-theme', 'light')
+    localStorage.setItem('theme', 'light')
+  } else {
+    bodyEl.setAttribute('data-theme', 'dark')
+    localStorage.setItem('theme', 'dark')
+  }
+})
